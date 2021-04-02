31f23b0e35550eca3b48f1c0650012f2b5523d62
James <gobot1234yt@gmail.com>
2021-04-02 23:58:50 +0100

    Add everything else

--- build.py
+++ build.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import shutil
 from typing import Any
 from pathlib import Path
@@ -7,10 +9,9 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 # make sure that the header files are copied to pybind's include folder before compilation
 INCLUDE = Path(pybind11.__file__).parent / "include"
-LOCATION = INCLUDE / "google"
 GOOGLE = Path().joinpath("protobuf", "src", "google").resolve()
 try:
-    shutil.move(GOOGLE, LOCATION)
+    shutil.move(GOOGLE, INCLUDE, copy_function=shutil.copytree)
 except FileNotFoundError:  # shouldn't happen in normal code however may happen during development
     pass
 
@@ -19,8 +20,10 @@ def build(setup_kwargs: dict[str, Any]) -> None:
     ext_modules = [
         Pybind11Extension("_protobuf_parser", ["protobuf_parser/protoc.cpp"]),
     ]
-    setup_kwargs.update({
-        'ext_modules': ext_modules,
-        'cmdclass': {'build_ext': build_ext},
-        "zip_safe": False,
-    })
+    setup_kwargs.update(
+        {
+            "ext_modules": ext_modules,
+            "cmdclass": {"build_ext": build_ext},
+            "zip_safe": False,
+        }
+    )
--- protobuf_parser/__init__.py
+++ protobuf_parser/__init__.py
@@ -1,10 +1,25 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import TYPE_CHECKING, AnyStr
+from typing import TYPE_CHECKING, AnyStr, Protocol
+
+from _protobuf_parser import run as _run, parse as _parse
 
 if TYPE_CHECKING:
     from _typeshed import SupportsRead
+
+__all__ = (
+    "Error",
+    "SyntaxError",
+    "Warning",
+    "parse",
+    "run",
+)
+
+
+class _SupportsStr(Protocol):
+    def __str__(self) -> str:
+        ...
 
 
 class Error(Exception):
@@ -14,28 +29,32 @@ class Error(Exception):
     message: str
 
 
-class SyntaxError(Error): ...
+class SyntaxError(Error):
+    ...
 
 
-class Warning(Error, Warning): ...
+class Warning(Error, Warning):
+    ...
 
 
 def parse(*files: SupportsRead[AnyStr]) -> tuple[bytes, list[Error]]:
     """Parse files using protoc.
-    
+
     Parameters
     ----------
     files: `SupportsRead`
         An object that has a read method.
-    
+
     Returns
     -------
     tuple[`bytes`, list[`Error`]]
         A tuple of the FileDescriptor's bytes and any errors that were encountered when parsing.
     """
+    output, errors = _parse(*files)
+    return output, [Error(error) for error in errors]
 
 
-def invoke_protoc(*args: str, **kwargs: str) -> list[Error]:
+def run(*args: _SupportsStr, **kwargs: _SupportsStr) -> list[Error]:
     """Manually invoke protoc.
 
     Parameters
@@ -58,4 +77,11 @@ def invoke_protoc(*args: str, **kwargs: str) -> list[Error]:
     list[`Error`]
         The errors that were encountered when invoking protoc.
     """
-    # encode strs
+
+    return [
+        Error(error)
+        for error in _run(
+            *[str(arg) for arg in args],
+            **{str(k): str(v) for k, v in kwargs}
+        )
+    ]
