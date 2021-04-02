aa90aedf0851f21ce1177a21bb3316c495b79459
James <gobot1234yt@gmail.com>
2021-04-02 23:51:08 +0100

    Add everything else

---
+++ protobuf_parser/protoc.cpp
@@ -0,0 +1,28 @@
+// Python version of this module of this is called _protobuf_parser
+
+#include <pybind11/pybind11.h>
+#include <google/protobuf/compiler/command_line_interface.h>
+#include <vector>
+
+namespace py = pybind11;
+
+struct Error {};
+struct SyntaxError : Error {};
+struct Warning : Error {};
+
+
+//std::tuple<const char, std::vector<Error>> parse(py::args files) {
+//
+//}
+
+//std::vector<Error> run(py::args args, py::kwargs kwargs) {
+//    std::vector
+//}
+
+PYBIND11_MODULE(_protobuf_parser, m) {
+    m.doc() = "Raw bindings for protoc";
+
+    py::class_<Error>(m, "Error");
+    py::class_<SyntaxError>(m, "SyntaxError");
+    py::class_<Warning>(m, "Warning");
+}
